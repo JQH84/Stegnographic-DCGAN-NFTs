750586a65a44693d3c4afdbe6daa7ef05f7ea261
Jihad Alhussain <jehad.alhussain@gmail.com>
2022-05-24 22:08:50 -0700

    nft contract , using openzepplin wizard

---
+++ nft.sol
@@ -0,0 +1,15 @@
+/*
+
+This contract will be an ERC-721 standrd contract to mint NFT's from the user on the blockchain 
+
+
+*/
+
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC721/ERC721Full.sol";
+
+contract 
+
+
+
