18aede2a1d8fcd2bca51bafde5fa457e7200cdb7
Jihad Alhussain <jehad.alhussain@gmail.com>
2022-05-25 20:31:01 -0700

    added hash funciton

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ from dotenv import load_dotenv  # to load the dot env file
 
 # Side Bar
 st.sidebar.title('Welcome !')
-st.sidebar.image(
-    'https://cdn.pixabay.com/photo/2021/11/26/10/45/nft-6825614_960_720.png', width=300)
+st.sidebar.image('https://cdn.pixabay.com/photo/2021/11/26/10/45/nft-6825614_960_720.png', width=300)
 
 option = st.sidebar.selectbox("Select Your Option", options=[
                               'Transform Image', 'Mint', 'Check encoded Message'])
@@ -26,27 +25,51 @@ file = st.file_uploader("Upload your image file to mint",
 
 # displaying the image uploaded on the screen
 if file is not None:
-    st.image(file, width=600)
+    st.image(file, width=300)
 
 
 # function to apply a filter on the image to change the color of the image
 def apply_filter(image):
     from PIL import Image, ImageFilter
     image = Image.open(image)
-    image = image.convert("L")
+    image = image.filter(ImageFilter.EMBOSS)
     return image
 
-# function to use the stegano library to encode a msg into an image 
-def encrypt_img(image , msg ):
-    
-    from stegano import lsb 
-    lsb.hide(image , msg).save('./encryotedimage.png')
 
-# function to decode that msg
-def decrypt_img(image):
-    from stegano import lsb
-    return lsb.reveal(image) 
-    
 # a button that will contain the minting logic function
 if st.button("Transform Your image"):
-    st.image(apply_filter(file), width=600)
+    st.image(apply_filter(file), width=300)
+
+# function to use the stegano library to encode a msg into an image
+
+
+def hide_msg(image, msg):
+    from stegano import lsb
+    lsb.hide(image, msg).save('./encryotedimage.png')
+
+
+# get the hidden input from the user and store in var
+input_txt = st.text_input('Type Here')
+
+
+def hash_input(txt):
+    import hashlib
+    sha = hashlib.sha256()
+    text = txt.encode()
+    sha.update(text)
+    return sha.hexdigest()
+
+
+if st.button('Enter a messege to embed in the image'):
+    hide_msg(image=apply_filter(file), msg=hash_input(input_txt))
+
+# function to decode that msg
+def get_msg(image):
+    from stegano import lsb
+    return lsb.reveal(image)
+
+
+# Action button to decrypt from the saved image
+if st.button('click to decrypt'):
+    decrypt = get_msg(image='./encryotedimage.png')
+    st.write(f'The Hidden Message is : "{decrypt}"')
